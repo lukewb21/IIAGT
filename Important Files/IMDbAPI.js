2ed61567ad2bd8d0decd7aabada480946df66e94
lukewb21 <96179585+lukewb21@users.noreply.github.com>
2022-12-05 19:55:39 +0000

    Update IMDbAPI.js

--- Important Files/IMDbAPI.js
+++ Important Files/IMDbAPI.js
@@ -1,17 +1,21 @@
 const axios = require("axios");
 
-const options = {
+const options3 = {
   method: 'GET',
-  url: 'https://mdblist.p.rapidapi.com/',
-  params: {s: 'Iron Man 2', m: 'movie'},
+  url: 'https://flixster.p.rapidapi.com/reviews/list',
+  params: {emsId: 'cbad9abb-8440-31a6-8caf-61ae45c2263b', limit: '20', offset: '0', withComments: 'true'},
   headers: {
     'X-RapidAPI-Key': '2ccadb270fmsh47baaaf5c69f6a1p12988cjsnab0cf8ac2ead',
-    'X-RapidAPI-Host': 'mdblist.p.rapidapi.com'
+    'X-RapidAPI-Host': 'flixster.p.rapidapi.com'
   }
 };
 
-axios.request(options).then(function (response) {
+axios.request(options3).then(function (response) {
 	console.log(response.data);
 }).catch(function (error) {
 	console.error(error);
 });
+
+let rating = options3[1];
+
+console.log('OUTPUT:', options3);
